0f012c92d7354ded33b91b0b934f403dbd84b15d
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-17 21:15:53 -0500

    wip

--- src/components/MyNavBar/MyNavBar.js
+++ src/components/MyNavBar/MyNavBar.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import PropTypes from 'prop-types';
+import Auth from '../Auth/Auth';
+import 'firebase/auth';
 
 class MyNavBar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool.isRequired,
+  }
+
+  logOutEvent = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
+    const { authed } = this.props;
     return (
       <div className="MyNavBar">
-        <h2>MyNavBar<i class="fas fa-heart"></i></h2>
+      <nav className="navbar fixed-top navbar-light bg-light">
+        <div className="navbar-brand" href="#">
+           Gwynterist
+          {
+            authed
+              ? <button className="btn btn-danger" onClick={this.logOutEvent}>Sign Out</button>
+              : <Auth />
+          }
+
+        </div>
+      </nav>
      </div>
     );
   }
