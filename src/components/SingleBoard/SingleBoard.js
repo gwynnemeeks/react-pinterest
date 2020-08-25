5eeaba206976ae5036446a1e9d5dba5f2b4fce95
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-24 21:41:37 -0500

    pins are added

--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Pin from '../Pins/Pins';
+import PinForm from '../PinForm/pinForm';
 
 import boardsData from '../../data/boardsData';
 import pinsData from '../../data/pinsData';
@@ -15,10 +16,12 @@ class SingleBoard extends React.Component {
     state = {
       board: {},
       pins: [],
+      showForm: false,
     }
 
     goGetYoPins = () => {
       const { boardId } = this.props;
+
       pinsData.getPinsByBoardId(boardId)
         .then((pins) => this.setState({ pins }))
         .catch((err) => console.error('get pins failed', err));
@@ -42,19 +45,30 @@ class SingleBoard extends React.Component {
         .catch((err) => console.error('delete pins failed', err));
     }
 
+    createPin = (newPin) => {
+      pinsData.createPin(newPin)
+        .then(() => {
+          this.goGetYoPins();
+          this.setState({ showForm: false });
+        })
+        .catch((err) => console.error(err));
+    }
+
     render() {
-      const { board, pins } = this.state;
-      const { setSingleBoard } = this.props;
+      const { board, pins, showForm } = this.state;
+      const { setSingleBoard, boardId } = this.props;
 
       const pinCards = pins.map((pin) => <Pin key={pin.id} pin={pin} deletePin={this.deletePin}/>);
 
       return (
         <div>
             <h4>{board.boardName}</h4>
-            <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>X</button>
+            <button className="btn btn-warning" onClick={() => { this.setState({ showForm: !showForm }); }}><i className={showForm ? 'far fa-times-circle' : 'far fa-plus-square'}></i></button>
+          {showForm ? <PinForm boardId={boardId} createPin={this.createPin} /> : ''}
             <div className="card-columns">
               {pinCards}
             </div>
+            <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>Go Back</button>
             </div>
       );
     }
