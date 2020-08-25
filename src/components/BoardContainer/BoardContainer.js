42b2b1f5fe3a72d4c563ef9c86e529811b290dbc
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-24 20:48:34 -0500

    create board works

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -2,10 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 import authData from '../../data/authData';
 import boardsData from '../../data/boardsData';
 import smashData from '../../data/smash';
+
+// create board
+// boardForm component
+// show board From on some click
+// need a button
+// go make the form on BoardForm
+// on submit of a form: save to firebase, make sure board shows up
 
 class BoardContainer extends React.Component {
   static propTypes = {
@@ -14,36 +22,50 @@ class BoardContainer extends React.Component {
 
   state = {
     boards: [],
+    formOpen: false,
   }
 
-goGetYoBoards = () => {
+getBoards = () => {
   boardsData.getBoardsByUid(authData.getUid())
     .then((boards) => this.setState({ boards }))
     .catch((err) => console.error('get boards broke', err));
 }
 
 componentDidMount() {
-  this.goGetYoBoards();
+  this.getBoards();
 }
 
 deleteBoard = (boardId) => {
   smashData.removeBoardAndPins(boardId)
     .then(() => {
-      this.goGetYoBoards();
+      this.getBoards();
     })
     .catch((err) => console.error('delete boards sucks', err));
 }
 
+createBoard = (newBoard) => {
+  boardsData.createBoard(newBoard)
+    .then(() => {
+      this.getBoards();
+      this.setState({ formOpen: false });
+    })
+    .catch((err) => console.error('create boards broke', err));
+}
+
 render() {
-  const { boards } = this.state;
+  const { boards, formOpen } = this.state;
   const { setSingleBoard } = this.props;
 
   const boardCard = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} deleteBoard={this.deleteBoard}/>);
 
   return (
+    <div>
+      <button className="btn btn-warning" onClick={() => { this.setState({ formOpen: !formOpen }); }}><i className="far fa-plus-square"></i></button>
+      { formOpen ? <BoardForm createBoard={this.createBoard}/> : '' }
       <div className="card-columns">
-        { boardCard }
-     </div>
+        {boardCard}
+      </div>
+    </div>
   );
 }
 }
