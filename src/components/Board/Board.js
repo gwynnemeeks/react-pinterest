7155753beaf9f4412d3f6bbcdb5a19b7da91597a
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-08-04 13:15:26 -0500

    wip sass

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './board.scss';
-
 import boardShape from '../../helpers/propz/boardShape';
 
 class Board extends React.Component {
